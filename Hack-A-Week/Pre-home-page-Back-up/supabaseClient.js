6b67d2f413a6476bdc0ac63f36f5983a72b7dc53
hulala1277 <randhirmukhiya2061@gmail.com>
2026-01-02 13:03:05 +0545

    csae

---
+++ Hack-A-Week/Pre-home-page-Back-up/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+    "https://sbumbhthxjnwkqwyecrl.supabase.co",
+    "sb_publishable_fWdR3zNDfIEkCsqxS9D0PA_O87eGktd"
+);
